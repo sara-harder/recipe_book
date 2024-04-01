88bc56d2083f9a21c96c978c377eaf3ccda67754
Sara Harder <harders@oregonstate.edu>
2024-04-01 12:09:11 +0200

    Added framework components to the home pages Used similar setup to mobile

--- RecipeBookWeb/src/pages/Home.js
+++ RecipeBookWeb/src/pages/Home.js
@@ -1,9 +1,71 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom"
 
-function HomePage() {
+const Recipe = ({title, image, nav}) => {
     return(
         <>
-            <div></div>
+            <div onClick={nav}>
+                <div>replace with {image}</div>
+                <h4>{title}</h4>
+            </div>
+        </>
+    )
+}
+
+const HorizontalRecipe = ({title, nav}) => {
+    const navigate = useNavigate()
+
+    const data = [{
+        img: "image_1",
+        title: "Recipe 1",
+        nav: ()=>navigate("/")
+    },
+    {
+        img: "image_2",
+        title: "Recipe 2",
+        nav: ()=>navigate("/")
+    },
+    {
+        img: "image_3",
+        title: "Recipe 3",
+        nav: ()=>navigate("/")
+    }]
+
+    return (
+        <>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>{title}</td>
+                            <td onClick={nav}>See All</td>
+                        </tr>
+                    </thead>
+                </table>
+                <table>
+                    <tbody>
+                        <tr>
+                            {data.map((item, index) => <td><Recipe title={item.title} image={item.img} nav={item.nav} key={index}/></td>)}
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </>
+    )
+}
+
+function HomePage() {
+    const navigate = useNavigate()
+
+    return(
+        <>
+            <h2>title</h2>
+            <div>
+                <HorizontalRecipe title="Favorites" nav={()=>navigate("/")} />
+                <HorizontalRecipe title="Recents" nav={()=>navigate("/")} />
+                <HorizontalRecipe title="Savory" nav={()=>navigate("/")} />
+                <HorizontalRecipe title="Sweet" nav={()=>navigate("/")} />
+            </div>
         </>
     )
 }
