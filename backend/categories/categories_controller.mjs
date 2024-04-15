092d8b4ab8a84274242698624590e02f55812eca
Sara Harder <harders@oregonstate.edu>
2024-04-15 22:33:47 +0200

    Add functions to categories controller

--- backend/categories/categories_controller.mjs
+++ backend/categories/categories_controller.mjs
@@ -7,4 +7,39 @@ const categoriesRouter = express.Router();
 categoriesRouter.use(bodyParser.json());
 
 
+categoriesRouter.post("/", asyncHandler(async(req, res, next) => {
+    // creates a category in the database
+    const category = await categories.createCategory(req.body.name, req.body.flavor_type)
+
+    if (category == null) res.type("application/json").status(500).send({Error: "Could not create category"})
+    res.type("application/json").status(201).send(category)
+}))
+
+categoriesRouter.get("/:id", asyncHandler(async(req, res, next) => {
+    // get the category based on the provided id
+    const id = {_id: req.params.id}
+    const category = await categories.getCategory(id)
+
+    if (category == null) res.type("application/json").status(404).send({Error: "Category not found"})
+    else res.type("application/json").status(200).send(category)
+}))
+
+categoriesRouter.get("/:flavor_type", asyncHandler(async(req, res, next) => {
+    // gets all of the categories in the specified flavor_type
+    const categories = await categories.getAllCategories(req.params.flavor_type)
+
+    if (categories == null) res.type("application/json").status(404).send({Error: "No categories found"})
+    else res.type("application/json").status(200).send(categories)
+}))
+
+categoriesRouter.delete("/:id", asyncHandler(async(req, res, next) => {
+    // delete the category with provided id
+    const id = {_id: req.params.id}
+    const count = await categories.deleteCategory(id)
+
+    if (count === 0) res.type("application/json").status(404).send({Error: "Category not found"})
+    else res.status(204).send()
+}))
+
+
 export default categoriesRouter;
