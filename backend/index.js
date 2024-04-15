fad83eefb78a2c0a4ff2804e9768582341711e04
Sara Harder <harders@oregonstate.edu>
2024-04-15 18:50:40 +0200

    Create user model schema and functions

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
 
+import usersRouter from './users/users_controller.mjs';
+import recipesRouter from './recipes/recipes_controller.mjs';
+import categoriesRouter from './categories/categories_controller.mjs';
+import reccatRouter from './recipe_in_category/recipe_cat_controller.mjs';
+
 const router = module.exports = express.Router();
 
+router.use('/users', usersRouter);
+router.use('/recipes', recipesRouter);
+router.use('/categories', categoriesRouter);
+router.use('/recipe-in-category', reccatRouter);
--- backend/users/users_model.mjs
+++ backend/users/users_model.mjs
@@ -1,1 +1,43 @@
 import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema({
+    // creates a Schema for the user table in the database
+    username: { type: String, required: true },
+    fullname: { type: String, required: true },
+    favorites: { type: Object, required: true },
+    recents: { type: Object, required: true },
+})
+
+// creates a User model class based on the precreated Schema
+const User = mongoose.model("User", userSchema)
+
+
+const createUser = async(username, fullname, favorites, recents) => {
+    // uses the User class to create a new user object
+    const user = new User({username, fullname, favorites, recents})
+    return user.save()
+}
+
+const getUser = async(filter) => {
+    // finds the user based on id
+    const query = User.find(filter)
+    return query.exec()
+}
+
+
+const updateUser = async(filter, update) => {
+    // finds the user based on id, then updates the provided criteria for that user
+    await User.updateOne(filter, update)
+    const query = User.find(filter)
+    return query.exec()
+}
+
+
+const deleteUser = async(filter) => {
+    // finds the user based on id and deletes them if in existence
+    const response = await User.deleteOne(filter)
+    return response.deletedCount
+}
+
+
+export {createUser, getUser, updateUser, deleteUser}
