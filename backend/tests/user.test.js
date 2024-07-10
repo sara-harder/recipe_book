0deda38f601d569a6094c70138ea6fa98fef0e8a
Sara Harder <harders@oregonstate.edu>
2024-07-10 10:18:31 +0200

    Updated tests to connect directly to mongoDB

--- backend/tests/user.test.js
+++ backend/tests/user.test.js
@@ -13,7 +13,7 @@ jest.setTimeout(60000);
 beforeAll(async () => {
     // create the connection to mongodb
     await mongoose.connect(
-        process.env.MONGODB_CONNECT_STRING
+        "mongodb+srv://harders:admin@recipes.fvmleot.mongodb.net/"
     );
 
     const db = mongoose.connection;
