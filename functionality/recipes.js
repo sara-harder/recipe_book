339343387fb49d347d6b77ee2b1e1a1743aef1f4
Sara Harder <harders@oregonstate.edu>
2024-07-13 17:05:30 +0200

    Created recipes.js file that accesses the backend Add the functions that correspond to the controller

---
+++ functionality/recipes.js
@@ -0,0 +1,65 @@
+async function addRecipe (new_recipe) {
+// creates a recipe in the database from the provided object. returns true if successful, false if not
+    try {
+        await fetch("/recipes", {
+            method: "POST", 
+            body: JSON.stringify(new_recipe),
+            headers: {"Content-type": "application/json"}
+        })
+        return true
+    } catch (error) { 
+        console.error(error)
+        return false
+    }
+}
+
+
+async function getRecipe (recipe_id) {
+// returns the recipe based on the provided id
+    let recipe;
+    try {
+        const response = await fetch(`/recipes/${recipe_id}`)
+        recipe = await response.json()
+    } catch (error) { console.error(error) }
+
+    return recipe
+}
+
+async function searchForRecipe (search) {
+// returns a list of recipes that match the provided search
+    let found_recipes;
+    try {
+        const response = await fetch(`/recipes/search/${search}`)
+        found_recipes = await response.json()
+    } catch (error) { console.error(error) }
+
+    return found_recipes
+}
+
+async function updateRecipe (recipe_id, updates) {
+// updates a recipe based on the provided id using the provided updates. returns true if successful, false if not
+    try {
+        await fetch(`/recipes/${recipe_id}`, {
+            method: "PUT", 
+            body: JSON.stringify(updates),
+            headers: {"Content-type": "application/json"}
+        })
+        return true
+    } catch (error) { 
+        console.error(error)
+        return false
+    }
+}
+
+async function deleteRecipe (recipe_id) {
+// deletes a recipe based on the provided id. returns true if successful, false if not
+    try {
+        await fetch(`/recipes/${recipe_id}`, {method: "DELETE"})
+        return true
+    } catch (error) { 
+        console.error(error)
+        return false
+    }
+}
+
+export {addRecipe, getRecipe, searchForRecipe, updateRecipe, deleteRecipe}
