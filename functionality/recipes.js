d11f55262e4aff236b27695fe23bf573d25f8823
Sara Harder <harders@oregonstate.edu>
2024-07-14 11:01:30 +0200

    Updated recipe.js to use better error handling Errors are now always printed correctly When an error arises the return value is set to undefined Installed Jest and dotenv

--- functionality/recipes.js
+++ functionality/recipes.js
@@ -1,14 +1,22 @@
+require('dotenv').config();
+const url = process.env.PROXY
+
 async function addRecipe (new_recipe) {
-// creates a recipe in the database from the provided object. returns recipe if successful, null if not
+// creates a recipe in the database from the provided object. returns recipe if successful, undef if not
     let recipe;
     try {
-        const response = await fetch("/recipes", {
+        const response = await fetch(url + "/recipes", {
             method: "POST", 
             body: JSON.stringify(new_recipe),
             headers: {"Content-type": "application/json"}
         })
         recipe = await response.json()
-    } catch (error) { console.error(error) }
+
+        if (recipe.Error) {throw (recipe.Error)}
+    } catch (error) { 
+        console.error(error)
+        recipe = undefined
+    }
 
     return recipe
 }
@@ -18,9 +26,14 @@ async function getRecipe (recipe_id) {
 // returns the recipe based on the provided id
     let recipe;
     try {
-        const response = await fetch(`/recipes/${recipe_id}`)
+        const response = await fetch(url + `/recipes/${recipe_id}`)
         recipe = await response.json()
-    } catch (error) { console.error(error) }
+
+        if (recipe.Error) {throw (recipe.Error)}
+    } catch (error) { 
+        console.error(error)
+        recipe = undefined
+    }
 
     return recipe
 }
@@ -29,24 +42,34 @@ async function searchForRecipe (search) {
 // returns a list of recipes that match the provided search
     let found_recipes;
     try {
-        const response = await fetch(`/recipes/search/${search}`)
+        const response = await fetch(url + `/recipes/search/${search}`)
         found_recipes = await response.json()
-    } catch (error) { console.error(error) }
+
+        if (found_recipes.Error) {throw (found_recipes.Error)}
+    } catch (error) { 
+        console.error(error)
+        found_recipes = undefined
+    }
 
     return found_recipes
 }
 
 async function updateRecipe (recipe_id, updates) {
-// updates a recipe based on the provided id using the provided updates. returns recipe if successful, null if not
+// updates a recipe based on the provided id using the provided updates. returns recipe if successful, undef if not
     let recipe;
     try {
-        const response = await fetch(`/recipes/${recipe_id}`, {
+        const response = await fetch(url + `/recipes/${recipe_id}`, {
             method: "PUT", 
             body: JSON.stringify(updates),
             headers: {"Content-type": "application/json"}
         })
         recipe = await response.json()
-    } catch (error) { console.error(error) }
+
+        if (recipe.Error) {throw (recipe.Error)}
+    } catch (error) { 
+        console.error(error)
+        recipe = undefined
+    }
 
     return recipe
 }
@@ -54,12 +77,16 @@ async function updateRecipe (recipe_id, updates) {
 async function deleteRecipe (recipe_id) {
 // deletes a recipe based on the provided id. returns true if successful, false if not
     try {
-        await fetch(`/recipes/${recipe_id}`, {method: "DELETE"})
-        return true
+        const res = await fetch(url + `/recipes/${recipe_id}`, {method: "DELETE"})
+        
+        if (res.status == 204) return true
+        else {
+            const response = await res.json()
+            throw (response.Error)
+        }
     } catch (error) { 
-        console.error(error)
         return false
     }
 }
 
-export {addRecipe, getRecipe, searchForRecipe, updateRecipe, deleteRecipe}
+module.exports = {addRecipe, getRecipe, searchForRecipe, updateRecipe, deleteRecipe}
