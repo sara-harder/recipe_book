948c5430c59a3780b614f1c40b519d7097d18551
Sara Harder <harders@oregonstate.edu>
2024-07-20 09:53:40 +0200

    Update functionality for mobile use Remove dotenv which isn't compatible on mobile Hard code base port instead Use 10.0.2.2 instead of localhost for mobile Add ResetPort for tests, which use unique ports

--- functionality/index.js
+++ functionality/index.js
@@ -1,3 +1,7 @@
-import {createFlexTable} from "./helpers"
+import {createFlexTable} from "./helpers.js"
+import * as user_funcs from "./backend_connection/users.js"
+import * as category_funcs from "./backend_connection/categories.js"
+import * as recipe_funcs from "./backend_connection/recipes.js"
+import * as rec_cat_funcs from "./backend_connection/recipes_in_categories.js"
 
-export {createFlexTable}
+export {createFlexTable, user_funcs, category_funcs, recipe_funcs, rec_cat_funcs}
