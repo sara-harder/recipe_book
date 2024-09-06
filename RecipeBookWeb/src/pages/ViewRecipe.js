ae7c589ce4785e6b77e7feca3b477e209ec4e766
Sara Harder <harders@oregonstate.edu>
2024-09-06 19:47:59 +0200

    Add fraction styling to ViewRecipe page

--- RecipeBookWeb/src/pages/ViewRecipe.js
+++ RecipeBookWeb/src/pages/ViewRecipe.js
@@ -28,6 +28,46 @@ function ViewRecipe({setHeader, setRecipe, setFavorite}) {
         setFavorite(favorites.includes(recipe._id))
     }, [])
 
+
+    const getFraction = (num, cls='fs-7 pe-1') => {
+        if (Math.abs(num - 0.75)    < 0.01) return [3, 4]
+        if (Math.abs(num - 0.666)   < 0.01) return [2, 3]
+        if (Math.abs(num - 0.5)     < 0.01) return [1, 2]
+        if (Math.abs(num - 0.333)   < 0.01) return [1, 3]
+        if (Math.abs(num - 0.25)    < 0.01) return [1, 4]
+        if (Math.abs(num - 0.166)   < 0.01) return [1, 6]
+        if (Math.abs(num - 0.125)   < 0.01) return [1, 8]
+        return num
+    }
+
+    const checkFraction = (num) => {
+        if (num == undefined) return undefined
+        if (num < 1) {
+            const frac = getFraction(num)
+            return(
+                <span className='pe-1'>
+                    <sup>{frac[0]}</sup>
+                    /
+                    <sub>{frac[1]}</sub>
+                </span>
+            )
+        }
+        if (num % 1 !== 0) {
+            const frac = getFraction(num % 1)
+            return (
+                <>
+                    {Math.floor(num/1)}
+                    <span className='fs-7 pe-1'>
+                        <sup>{frac[0]}</sup>
+                        /
+                        <sub>{frac[1]}</sub>
+                    </span>
+                </>
+            )
+        }
+        return num
+    }
+
     return(
         <Container fluid>
             <Row className='bg-color3 my-3 py-3 fs-5'>
@@ -36,13 +76,13 @@ function ViewRecipe({setHeader, setRecipe, setFavorite}) {
                     <ul className='list-unstyled'>
                         {ingredients.map((item, index) => 
                             <li className='row' key={index}>
-                                <Col className='col-4 right' >{item.quantity}{item.unit}  </Col>
+                                <Col className='col-4 right' >{checkFraction(item.quantity)}{item.unit}</Col>
                                 <Col className='col-8 left' >{item.name}</Col>
                             </li>
                         )}
                     </ul>
                 </Col>
-                
+
                 <Col xs={7} md={8} lg={9} className='border-start border-primary'>
                     <h4 className='fw-bold px-3'>Directions:</h4>
                     <ul>
