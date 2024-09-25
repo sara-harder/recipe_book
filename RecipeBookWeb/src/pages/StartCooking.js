c98adb4a5127a3d15621e6672046b2579aaa4336
Sara Harder <harders@oregonstate.edu>
2024-09-25 14:04:21 +0200

    Improve styling on StartCooking page Now adjust with height changes Separate the ingredients into two sections when no ingredient highlighted

--- RecipeBookWeb/src/pages/StartCooking.js
+++ RecipeBookWeb/src/pages/StartCooking.js
@@ -18,21 +18,51 @@ function StartCooking() {
 
     const [selected, setSelected] = useState(0)
     const [connected, setConnected] = useState([])
+    const [lastSelected, setLastSelected] = useState(-1)
+    const [lastConnected, setLastConnected] = useState([])
+
+    const onNext = () => {
+        setLastSelected(selected)
+        setSelected(selected + 1)
+    }
+
+    const onPrevious = () => {
+        setLastSelected(selected)
+        setSelected(selected - 1)
+    }
 
     useEffect(() => {
-        if (recipe.connections[selected] != undefined){
-            setConnected(recipe.connections[selected])
+        // if moved backwards while ingredients connected, pop last connected ingredient index
+        const copy = lastConnected.slice()
+        if (connected.length != 0) {
+            if(lastSelected > selected) {
+                copy.pop()
+            }
+        }
+
+        // retrieve new connections for newly selected direction
+        const connections = recipe.connections[selected]
+        if (connections != undefined){
+            setConnected(connections)
+
+            // if moved forward and new direction has connections, push last connected ingredient index
+            if (lastSelected < selected) {
+                copy.push(connections[connections.length - 1])
+            }
         } else setConnected([])
+
+        // update stack of last connected ingredient indexes
+        setLastConnected(copy)
     }, [selected])
 
     return(
         <Container fluid>
-            <Row className='fs-5'>
-                <Col xs={4} className=''>
-                    <Row className='h-75'>
-                        <ul className={`list-unstyled overflow-y-scroll ${connected[0] == undefined ? 'center-vertical' : 'align-top h-25'}`}>
+            <Row className='fs-55 cooking-page'>
+                <Col xs={4} className='h-100'>
+                    <Row className='cooking-display overflow-hidden'>
+                        <ul className='list-unstyled align-bottom m-0 cooking-begin'>
                             <Col>
-                            {recipe.ingredients.slice(0, connected[0]).map((item, index) => 
+                            {recipe.ingredients.slice(0, connected[0] != undefined ? connected[0] : lastConnected[lastConnected.length - 1] + 1).map((item, index) => 
                                 <li className='row' key={index}>
                                     <Col className='col-5 right text-nowrap overflow-hidden' >{checkFraction(item.quantity)}{item.unit}</Col>
                                     <Col className='col-7 left' >{item.name}</Col>
@@ -40,56 +70,52 @@ function StartCooking() {
                             )}
                             </Col>
                         </ul>
-                        {connected[0] != undefined ? 
-                        <>
-                            <ul className='list-unstyled center-vertical h-50'>
-                                <Col>
-                                {connected.map((idx) => 
-                                    <li className='row fs-3 fw-bold' key={idx}>
-                                        <Col className='col-5 right text-nowrap overflow-hidden' >{checkFraction(recipe.ingredients[idx].quantity)}{recipe.ingredients[idx].unit}</Col>
-                                        <Col className='col-7 left' >{recipe.ingredients[idx].name}</Col>
-                                    </li>
-                                )}
-                                </Col>
-                            </ul>
-                            <ul className='list-unstyled align-bottom h-25 overflow-y-scroll'>
-                                {recipe.ingredients.slice(connected[0]).map((item, index) => 
-                                    connected.includes(index + connected[0]) ? null : 
-                                    <li className='row' key={index}>
-                                        <Col className='col-5 right text-nowrap overflow-hidden' >{checkFraction(item.quantity)}{item.unit}</Col>
-                                        <Col className='col-7 left' >{item.name}</Col>
-                                    </li>
-                                )}
-                            </ul>
-                        </>
-                        : null }
+                        <ul className='list-unstyled center-vertical m-0 cooking-focused'>
+                            <Col>
+                            {connected.map((idx) => 
+                                <li className='row fs-3 fw-bold' key={idx}>
+                                    <Col className='col-5 right text-nowrap' >{checkFraction(recipe.ingredients[idx].quantity)}{recipe.ingredients[idx].unit}</Col>
+                                    <Col className='col-7 left' >{recipe.ingredients[idx].name}</Col>
+                                </li>
+                            )}
+                            </Col>
+                        </ul>
+                        <ul className='list-unstyled overflow-hidden cooking-end'>
+                            {recipe.ingredients.slice(lastConnected[lastConnected.length - 1] + 1).map((item, index) => 
+                                <li className='row' key={index}>
+                                    <Col className='col-5 right text-nowrap' >{checkFraction(item.quantity)}{item.unit}</Col>
+                                    <Col className='col-7 left' >{item.name}</Col>
+                                </li>
+                            )}
+                        </ul>
                     </Row>
                 </Col>
-                <Col xs={8}>
-                    <Row className='h-75'>
-                        <ul className='list-unstyled align-top h-25 overflow-y-scroll'>
+                <Col xs={8} className='h-100'>
+                    <Row className='cooking-display overflow-hidden'>
+                        <ul className='list-unstyled align-bottom m-0 cooking-begin'>
+                            <Col>
                             {recipe.directions.slice(0, selected).map((item, index) => 
                                 <li key={index}>{item}</li>
                             )}
+                            </Col>
                         </ul>
-                        <ul className='list-unstyled center-vertical h-50'>
+                        <ul className='list-unstyled center-vertical m-0 cooking-focused'>
                             <li className='fs-3 fw-bold py-5'>{recipe.directions[selected]}</li>
                         </ul>
-                        <ul className='list-unstyled align-bottom h-25 overflow-y-scroll'>
+                        <ul className='list-unstyled cooking-end'>
                             {recipe.directions.slice(selected + 1).map((item, index) => 
                                 <li key={index}>{item}</li>
                             )}
                         </ul>
-                        <div className='p-5'></div>
                     </Row>
-                    <Row>
-                        <Col className='py-5 px-3 left'>
-                            <Button variant="success" type="button" className='bg-color5 border-color5' disabled={selected <= 0} onClick={() => setSelected(selected-1)}>
+                    <Row className='position-relative z-2 center-content cooking-buttons'>
+                        <Col className='px-3 left'>
+                            <Button variant="success" type="button" className='bg-color5 border-color5' disabled={selected <= 0} onClick={onPrevious}>
                                 Previous
                             </Button>
                         </Col>
-                        <Col className='py-5 px-4 right'>
-                            <Button variant="success" type="button" className='bg-color5 border-color5' disabled={selected >= recipe.directions.length - 1} onClick={() => setSelected(selected+1)}>
+                        <Col className='px-4 right'>
+                            <Button variant="success" type="button" className='bg-color5 border-color5' disabled={selected >= recipe.directions.length - 1} onClick={onNext}>
                                 Next
                             </Button>
                         </Col>
